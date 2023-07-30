96f3196cdcb62ae63f0131f2225cd899f2cd6b2f
shenhuaxin <shx_jx@163.com>
2023-07-30 19:35:04 +0800

    feat: 提交代码

--- src/components/Condition-Editor.vue
+++ src/components/Condition-Editor.vue
@@ -1,80 +1,196 @@
 <template>
-  <div>
-    <n-space>
-    <n-tag>就是</n-tag>
-    </n-space>
+  <div class="app-content">
+    <div style="height: 400px" id="container"></div>
+    <TeleportContainer/>
   </div>
-  <div id="mountNode"></div>
 </template>
 
-<script>
-import G6 from "@antv/g6";
-import NTag from "naive-ui";
 
-export default {
+<script>
+import {defineComponent} from 'vue'
+import {Graph, Shape} from '@antv/x6'
+import {register, getTeleport} from '@antv/x6-vue-shape'
+import TagNode from "@/components/TagNode.vue";
+import Hierarchy from "@antv/hierarchy";
+
+register({
+  shape: 'custom-vue-node',
+  width: 100,
+  height: 100,
+  component: TagNode,
+})
+Graph.registerEdge(
+    'mindmap-edge',
+    {
+      inherit: 'edge',
+      router: {
+        name: 'manhattan',
+        args: {
+          startDirections: ['right'],
+          endDirections: ['left']
+        }
+      },
+      connector: {
+        name: 'rounded'
+      },
+      attrs: {}, //样式代码
+      zIndex: 0
+    },
+    true,
+)
+const TeleportContainer = getTeleport()
+
+let graph = null;
+export default defineComponent({
   name: 'ConditionEditor',
-  props: {
-    msg: String
-  },
   components: {
-    NTag
+    TeleportContainer,
   },
   mounted() {
-    initGraph()
+    graph = new Graph({
+      container: document.getElementById('container'),
+      grid: true,
+      mousewheel: {
+        enabled: true,
+        zoomAtMousePosition: true,
+        modifiers: 'ctrl',
+        minScale: 0.5,
+        maxScale: 3,
+      },
+      connecting: {
+        router: 'manhattan',
+        connector: {
+          name: 'rounded',
+          args: {
+            radius: 8,
+          },
+        },
+        anchor: 'center',
+        connectionPoint: 'anchor',
+        allowBlank: false,
+        snap: {
+          radius: 20,
+        },
+        createEdge() {
+          return new Shape.Edge({
+            attrs: {
+              line: {
+                stroke: '#A2B1C3',
+                strokeWidth: 2,
+                targetMarker: {
+                  name: 'block',
+                  width: 12,
+                  height: 8,
+                },
+              },
+            },
+            zIndex: 0,
+          })
+        },
+        validateConnection({ targetMagnet }) {
+          return !!targetMagnet
+        },
+      },
+      highlighting: {
+        magnetAdsorbed: {
+          name: 'stroke',
+          args: {
+            attrs: {
+              fill: '#5F95FF',
+              stroke: '#5F95FF',
+            },
+          },
+        },
+      },
+    })
+    render()
+  },
+})
+var data = {
+  id: "1",
+  data: {
+    text: "AND"
+  },
+  children: [
+    {
+      id: "2",
+      data: {
+        text: "indCode = '123'"
+      }
+    },
+    {
+      id: "3",
+      data: {
+        text: "dataType = 1"
+      }
+    }
+  ]
+}
+// eslint-disable-next-line no-unused-vars
+const render = () => {
+  const result = Hierarchy.mindmap(data, {
+    direction: 'H',
+    getHeight(d) {
+      return d.height
+    },
+    getWidth(d) {
+      return d.width
+    },
+    getHGap() {
+      return 40
+    },
+    getVGap() {
+      return 20
+    },
+    getSide: () => {
+      return 'right'
+    },
+  })
+  const cells = []
+  const traverse = (hierarchyItem) => {
+    if (hierarchyItem) {
+      const {data, children} = hierarchyItem
+      cells.push(
+          graph.createNode({
+            id: data.id,
+            shape: 'custom-vue-node',
+            width: data.width,
+            height: data.height,
+            data: data.data
+          }),
+      )
+      if (children) {
+        children.forEach((item) => {
+          const {id} = item
+          cells.push(
+              graph.createEdge({
+                source: {
+                  cell: data.id,
+                  anchor: {
+                    name: 'right',
+                    args: {
+                      dx: '25%'
+                    }
+                  }
+                },
+                target: {
+                  cell: id,
+                  anchor: {
+                    name: 'left'
+                  }
+                },
+                sharp: 'mindmap-edge'
+              }))
+          traverse(item)
+        })
+      }
+    }
   }
+  traverse(result)
+  graph.resetCells(cells)
+  graph.centerContent()
+  graph.zoomToFit({ padding: 10, maxScale: 1 })
 }
-const data = {
-  // 点集
-  nodes: [
-    {
-      id: 'node1', // String，该节点存在则必须，节点的唯一标识
-      x: 100, // Number，可选，节点位置的 x 值
-      y: 200, // Number，可选，节点位置的 y 值
-    },
-    {
-      id: 'node2', // String，该节点存在则必须，节点的唯一标识
-      x: 300, // Number，可选，节点位置的 x 值
-      y: 200, // Number，可选，节点位置的 y 值
-    },
-  ],
-  // 边集
-  edges: [
-    {
-      source: 'node1', // String，必须，起始点 id
-      target: 'node2', // String，必须，目标点 id
-    },
-  ],
-};
-
-
-G6.registerNode(
-    'rect-xml',
-    () => `
-  <rect style={{
-    width: 100, height: 20
-  }} keyshape="true" name="test">
-    <text
-			name="title">你好</text>
-  </rect>
-`,
-);
-
-
-function initGraph() {
-  const graph = new G6.Graph({
-    container: 'mountNode', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
-    width: 800, // Number，必须，图的宽度
-    height: 500, // Number，必须，图的高度
-    defaultNode: {
-      type: 'rect-xml',
-      size: [120, 40],
-    },
-  });
-  graph.data(data); // 读取 Step 2 中的数据源到图上
-  graph.render(); // 渲染图
-}
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/TagNode.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <n-tag closable>{{text}}</n-tag>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import {NTag} from 'naive-ui'
+export default defineComponent({
+  name: 'ProgressNode',
+  inject: ['getNode'],
+  components: {
+    NTag
+  },
+  data() {
+    return {
+      text: ""
+    }
+  },
+  mounted() {
+    const node = this.getNode()
+    this.text = node.getData().text
+  },
+})</script>
+
+
+<style scoped>
+
+</style>
